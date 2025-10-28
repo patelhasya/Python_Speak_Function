f3964b8b6d0f5bf5cb2897245a823e6e130448d0
Cosmo <121879395+patelhasya@users.noreply.github.com>
2025-10-28 23:20:38 +0530

    Add files via upload

---
+++ Speak.py
@@ -0,0 +1,20 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+
+
+# speak("hello")
+
